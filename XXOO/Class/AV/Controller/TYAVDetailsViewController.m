10e843533c8f36e8963dc00874473c981779ec1d
吴棒棒 <302857194@qq.com>
2019-04-12 11:20:12 +0800

    代码提交

--- XXOO/Class/AV/Controller/TYAVDetailsViewController.m
+++ XXOO/Class/AV/Controller/TYAVDetailsViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "TYAVDetailsViewController.h"
+#import "SJVideoPlayer.h"
+#import <SJUIKit/SJAttributeWorker.h>
 
-@interface TYAVDetailsViewController ()
+static SJEdgeControlButtonItemTag SJEdgeControlButtonItemTag_Share = 10;        // 分享
+
+@interface TYAVDetailsViewController ()<SJEdgeControlButtonItemDelegate>
+@property (nonatomic, strong) SJVideoPlayer *player;
 
 @end
 
@@ -17,16 +22,88 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    self.edgesForExtendedLayout = UIRectEdgeNone;
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    // create a player of the default type
+    _player = [SJVideoPlayer player];
+    
+    [self.view addSubview:_player.view];
+    [_player.view mas_makeConstraints:^(MASConstraintMaker *make) {
+        if (@available(iOS 11.0, *)) make.top.equalTo(self.view.mas_safeAreaLayoutGuideTop);
+        else make.top.offset(0);
+        make.leading.trailing.offset(0);
+        make.height.equalTo(self->_player.view.mas_width).multipliedBy(9 / 16.0f);
+    }];
+    
+    _player.pausedToKeepAppearState = YES;
+    _player.URLAsset = [[SJVideoPlayerURLAsset alloc] initWithURL:[NSURL URLWithString:@"https://xy2.v.netease.com/2018/0815/d08adab31cc9e6ce36111afc8a92c937qt.mp4"]];
+    _player.URLAsset.title = @"十五年前, 一见钟情";
+    NSTimeInterval secs = 20.0;
+    _player.URLAsset.specifyStartTime = secs;
+    
+    [_player showTitle:@"当前Demo为: 更多 item 的创建示例" duration:-1];
+    
+    
+    // 1. 49 * 49 大小的图片item
+    SJEdgeControlButtonItem *imageItem = [[SJEdgeControlButtonItem alloc] initWithImage:[UIImage imageNamed:@"share"] target:self action:@selector(test:) tag:SJEdgeControlButtonItemTag_Share];
+    /// 是否隐藏
+    /// 有些时候, 我们需要某个按钮, 在小屏时隐藏, 大屏后显示.
+    /// 可以通过下面这个属性, 来控制item是否隐藏
+    /// 注意: 将逻辑放到item的代理方法中`updatePropertiesIfNeeded:videoPlayer:`
+    imageItem.delegate = self;
+    //    imageItem.hidden = YES;
+    [_player.defaultEdgeControlLayer.topAdapter addItem:imageItem];
+    
+    
+    // 2. 49 * title.size.width
+    SJEdgeControlButtonItem *titleItem = [[SJEdgeControlButtonItem alloc] initWithTitle:sj_makeAttributesString(^(SJAttributeWorker * _Nonnull make) {
+        make.append(@"Share").font([UIFont systemFontOfSize:14]).textColor([UIColor whiteColor]).alignment(NSTextAlignmentCenter);
+    }) target:self action:@selector(test:) tag:SJEdgeControlButtonItemTag_Share];
+    
+    // 调整 item 前后间隔
+    titleItem.insets = SJEdgeInsetsMake(8, 8);
+    [_player.defaultEdgeControlLayer.topAdapter addItem:titleItem];
+    
+    
+    
+    [_player.defaultEdgeControlLayer.topAdapter reload];
+    
+    
+    
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)test:(SJEdgeControlButtonItem *)item {
+    
 }
-*/
+
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    [self.player vc_viewDidAppear];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    [self.player vc_viewWillDisappear];
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+    [self.player vc_viewDidDisappear];
+}
+
+- (BOOL)prefersStatusBarHidden {
+    return [self.player vc_prefersStatusBarHidden];
+}
+
+- (UIStatusBarStyle)preferredStatusBarStyle {
+    return [self.player vc_preferredStatusBarStyle];
+}
+
+- (BOOL)prefersHomeIndicatorAutoHidden {
+    return YES;
+}
 
 @end
